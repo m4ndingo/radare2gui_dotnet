1c3d6e1b92e84235fcadf37c5a51184601838817
yourusername <yourusername>
2016-12-07 20:43:12 +0100

    new class

---
+++ r2pipe_test/Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r2pipe_test
+{
+    public class Benchmarks
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
+        public Benchmarks()
+        {
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+        public float getCurrentCpuUsage(){
+            return cpuCounter.NextValue();
+        }
+        public string getAvailableRAM(){
+            return ramCounter.NextValue()+"MB";
+        }
+    }
+}
