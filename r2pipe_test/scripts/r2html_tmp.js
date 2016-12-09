d7c92d41fb77058ffbc84ebf5915716ae2c8e9a8
yourusername <yourusername>
2016-12-10 00:40:01 +0100

    added scripts and media to zip file for easy download

--- r2pipe_test/scripts/r2html_tmp.js
+++
@@ -1,8 +0,0 @@
-function dump_json(jsondata)
-{
-    document.write("<span class=r2code>");
-    document.write(JSON.stringify(jsondata, null, "   "));
-    document.write("</span>");
-}
-
-if (r2output != null && typeof(r2output) == "object") dump_json(r2output);
